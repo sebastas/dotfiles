5c355feb49b3b901c1750ff8b91c5dd5d2846ee7
Sebastian Slettebakken <43045439+sebastas@users.noreply.github.com>
2022-11-23 18:23:38 +0100

    update outputs formatting

--- fish/functions/outputs.fish
+++ fish/functions/outputs.fish
@@ -1,5 +1,5 @@
 function info -d 'displays argument as information'
-    echo [(set_color --bold) ' !! ' (set_color normal)] $argv
+    echo [(set_color yellow) ' !! ' (set_color normal)] $argv
 end
 
 function user -d 'displays argument as a prompt for input'
@@ -16,9 +16,9 @@ function fail -d 'displays argument as a warning and exits'
 end
 
 function title -d 'displays argument as a title'
-    echo (set_color cyan) '========== ' $argv ' ==========' (set_color normal)
+    echo (set_color cyan)'[========== ' $argv ' ==========]'(set_color normal)
 end
 
 function subtitle -d 'displays argument as a subtitle'
-    echo (set_color magenta) '===== ' $argv ' =====' (set_color normal)
+    echo (set_color magenta)'[======= ' $argv ' =======]'(set_color normal)
 end
