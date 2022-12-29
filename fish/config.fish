5495d8fd9145c41752b21e4e3779a4b646bcc161
Sebastian Slettebakken <sesl@equinor.com>
2022-12-29 14:43:42 +0100

    Add fzf options to fish/config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -2,4 +2,12 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     set fish_greeting "" # disable greeting
     fish_add_path $HOME/.local/bin # add to PATH for bat and fd
+
+    # fzf-fish config
+    set fzf_fd_opts --hidden --exclude={.git,.vscode,.vscode-server,.idea}
+    set -gx EDITOR "micro"
+    set fzf_directory_opts --bind "ctrl-o:execute($EDITOR {} &> /dev/tty)"
+
+    # LS_COLORS (using vivid - https://github.com/sharkdp/vivid)
+    set -gx LS_COLORS (vivid generate nord)
 end
