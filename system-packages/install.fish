ece843d6183bb373a7977752c598511c0bc7847e
Sebastian Slettebakken <43045439+sebastas@users.noreply.github.com>
2023-01-01 17:34:20 +0100

    fix vivid installation

--- system-packages/install.fish
+++ system-packages/install.fish
@@ -23,10 +23,36 @@ function install_apt
     end
 end
 
+# vivid is not available in apt
+function install_vivid
+    if test -e /usr/bin/vivid
+        set -l latest_vivid (curl -sL https://api.github.com/repos/sharkdp/vivid/releases/latest | jq -r ".tag_name")
+        or fail "unable to retrieve the latest vivid version..."
+
+        set -l current_vivid v(vivid --version | awk '{print $2}')
+        if test $current_vivid = $latest_vivid
+            success "vivid is already installed and up-to-date [$current_vivid]"
+            exit 0
+        end
+    end
+    info "installing vivid..."
+    set -l latest_vivid_download (curl -sL https://api.github.com/repos/sharkdp/vivid/releases/latest | jq -r '.assets[] | select(.name|match("amd64.deb")) | select(.name|contains("musl")|not) | .browser_download_url')
+    or fail "unable to retrieve latest vivid release..."
+    wget $latest_vivid_download -O vivid.deb &>/dev/null
+    sudo dpkg -i vivid.deb &>/dev/null
+    rm vivid.deb
+    if test -e /usr/bin/vivid
+        success "installed vivid"
+    else
+        fail "vivid wasn't installed"
+    end
+end
+
 if test (uname) = Linux
     switch $distro
         case ubuntu debian pop
             install_apt
+            install_vivid
         case '*'
             info "unable to install packages for [$distro]"
     end
